892d0d7041b9dd4ae3d1528ad1756513288a96ad
AufaRafiki2003 <aufarafiki2003@gmail.com>
2024-01-22 11:35:02 +0700

    menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,40 +113,13 @@
                             </li>
 
                             @if(Auth::user()->role == 'admin') <!-- untuk admin mulai dari sini -->
-                            <li class="mr-3 flex-1">
-                                <a href=""
-                                    class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                                    <i class="fa fa-address-card pr-0 md:pr-3"></i><span
-                                        class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">MAHASISWA</span>
-                                </a>
-                            </li>
-                            <li class="mr-3 flex-1">
-                                <a href="{{ route('admin.bank.index') }}"
-                                    class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800  hover:border-pink-500">
-                                    <i class="fa fa-address-card pr-0 md:pr-3"></i><span
-                                        class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">DETAIL BANK</span>
-                                </a>
-                            </li>
-                            <li class="mr-3 flex-1">
-                                <a href=""
-                                    class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                                    <i class="fa fa-address-card pr-0 md:pr-3"></i><span
-                                        class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">PEMBAYARAN</span>
-                                </a>
-                            </li>
+                        
 
 
                                 @endif <!-- sampe sini  -->
 
                                 @if(Auth::user()->role == 'saksi') <!-- untuk saksi mulai dari sini -->
-                            </li>
-                            <li class="mr-3 flex-1">
-                                <a href=""
-                                    class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                                    <i class="fa fa-address-card pr-0 md:pr-3"></i><span
-                                        class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">PEMBAYARAN</span>
-                                </a>
-
+                         
                             @endif <!-- sampe sini  -->
 
 
